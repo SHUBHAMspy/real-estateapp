55ec98bd59a5212f0e356876079d899bc7f25dad
Shubham Pandey <shubhampandey1964@gmail.com>
2022-09-11 17:50:46 +0530

    Added Searchbar

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,24 +1,33 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { GlobalContext } from "../../context/SearchContext";
 import './style.css';
 
 function SearchBar() {
+  const {countries,country,addCity,priceRange,propertyType} = useContext(GlobalContext)
   const [cityIsOpen, setCityIsOpen] = useState(false);
   const [dateIsOpen, setDateIsOpen] = useState(false);
   const [priceIsOpen, setPriceIsOpen] = useState(false);
   const [propertyIsOpen, setPropertyIsOpen] = useState(false);
-  
+  console.log(countries);
   return (
     <div className="search-bar ">
       <div className="search-filter">
         <div className="search-dropdown-btn" onClick={() => setCityIsOpen(!cityIsOpen)}>
           <div>
-            <div className='search-text'>India</div>
+            <div className='search-text'>{country}</div>
             <div className='label-text'>Select your place</div>
           </div>
           {cityIsOpen 
             ? <ion-icon name="chevron-up-outline" className="remove-icon" ></ion-icon>
             : <ion-icon name="chevron-down-outline" className="add-icon"  ></ion-icon>
           }
+          <div className="search-dropdown-content">
+            { 
+              countries.map((country,index) => (
+                <p key={index} className="content-item">{country}</p>
+              ))
+            }
+          </div>
         </div>
         <div className="search-dropdown-btn" onClick={() => setDateIsOpen(!dateIsOpen)}>
           <div>
@@ -29,6 +38,7 @@ function SearchBar() {
             ? <ion-icon name="chevron-up-outline" className="remove-icon" ></ion-icon>
             : <ion-icon name="chevron-down-outline" className="add-icon"  ></ion-icon>
           }
+          
         </div>
         <div className="search-dropdown-btn" onClick={() => setPriceIsOpen(!priceIsOpen)}>
           <div>
@@ -49,6 +59,14 @@ function SearchBar() {
             ? <ion-icon name="chevron-up-outline" className="remove-icon" ></ion-icon>
             : <ion-icon name="chevron-down-outline" className="add-icon"  ></ion-icon>
           }
+          <div className="search-dropdown-content">
+            { 
+              
+              countries.map((country,index) => (
+                <div className="content-item">{country}</div>
+              ))
+            }
+          </div>
         </div>
         <button className="search-button">Search</button>
       </div>
